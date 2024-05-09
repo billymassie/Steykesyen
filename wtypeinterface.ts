41b025bfeabee7ac606359208ad9f8342205e625
billy.massie <billybois68@gmail.com>
2024-05-09 20:36:51 +0700

    feat: properties & property card

---
+++ wtypeinterface.ts
@@ -0,0 +1,30 @@
+interface IProperties {
+  _id: string;
+  owner: string;
+  name: string;
+  type: string;
+  description: string;
+  location: {
+    street: string;
+    city: string;
+    state: string;
+    zipcode: string;
+  };
+  beds: number;
+  baths: number;
+  square_feet: number;
+  amenities: [string];
+  rates: {
+    weekly: number;
+    monthly: number;
+  };
+  seller_info: {
+    name: string;
+    email: string;
+    phone: string;
+  };
+  images: [string];
+  is_featured: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
