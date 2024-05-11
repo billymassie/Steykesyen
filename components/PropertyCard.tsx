5b266075c232efcb034c45650274b30dac7a295d
billy.massie <billybois68@gmail.com>
2024-05-11 10:38:35 +0700

    feat: property detail page

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -1,4 +1,4 @@
-import { IProperties } from '@/app/properties/interface';
+import { IProperties } from '@/types/Properties';
 import Image from 'next/image';
 import Link from 'next/link';
 import { FaBed, FaBath, FaRulerCombined, FaMoneyBill, FaMapMarker } from 'react-icons/fa';
@@ -78,7 +78,7 @@ const PropertyCard = ({ property }: { property: IProperties }) => {
             </span>
           </div>
           <Link
-            href={`/property/${property._id}`}
+            href={`/properties/${property._id}`}
             className='h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm'
           >
             Details
--- utils/request.ts
+++ utils/request.ts
@@ -1,5 +1,6 @@
 import { IProperties } from '@/types/Properties';
 
+//fetch all properties
 export async function fetchProperties(): Promise<IProperties[]> {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
@@ -12,5 +13,20 @@ export async function fetchProperties(): Promise<IProperties[]> {
   } catch (error) {
     console.log(error);
     return [];
+  }
+}
+//fetch single property
+export async function fetchProperty(id: string | string[]): Promise<IProperties | null> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error('Failde to fetch data');
+    }
+    const result = (await res.json()) as IProperties;
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
   }
 }
